20238be56be692358f32babbe1a5c13a28fa36cb
valentincerezuela04 <valentincerezuela04@gmail.com>
2025-08-16 17:05:00 -0300

    Template string

---
+++ 01-reforzamiento/src/bases/02-template-string.ts
@@ -0,0 +1,9 @@
+const Name = 'valentin';
+const lastNamee = 'cerezuela';
+
+const fullName = 
+`
+The name is ${Name} ${lastNamee}
+`;
+
+console.log(fullName);
--- 01-reforzamiento/src/main.tsx
+++ 01-reforzamiento/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import './bases/01-const-let.ts'
+import './bases/02-template-string.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
