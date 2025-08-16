21de730e641c64c22432e806acb25cddc5d7c4f5
valentincerezuela04 <valentincerezuela04@gmail.com>
2025-08-16 18:32:50 -0300

    interfaces

--- 01-reforzamiento/src/bases/03-object-literal.ts
+++ 01-reforzamiento/src/bases/03-object-literal.ts
@@ -1,4 +1,17 @@
-const person = {
+interface Person{
+    name:string,
+    lastName:string,
+    age:number,
+    address : Address //? is a optional object
+}
+
+interface Address{
+    postalCode:string,
+    city:string
+}
+
+
+const ironMan: Person = {
     name: 'Tony',
     lastName: 'stark',
     age: 45,
@@ -8,9 +21,19 @@ const person = {
     }
 };
 
+
+// if we use optional address "?" we can define an object like this >
+
+// const hulk : Person = {
+//     name: "Hulk",
+//     lastName: "Hulk",
+//     age: 0,
+
+// }
+
 //we can't use object like this:
 
-// person = {
+// ironMan = {
 //     name: 'Peter',
 //     lastName: 'parker',
 //     age: 45
@@ -19,15 +42,15 @@ const person = {
 
 //instead (we should use objects with const ):
 
-const spiderMan = structuredClone(person);
+const spiderMan = structuredClone(ironMan);
 spiderMan.name = 'peter';
 spiderMan.lastName = 'parker';
 spiderMan.address.city = 'New York';
 
-//const spiderMan = {...person}; making a copy like this works but ,does't work if we have another object inside another
-//because when we modify spiderman.addres.city to New York the city atribute in person would change to new york
-//so basically we don't want that so instead wue should make a copy with structuredClone(person)
+//const spiderMan = {...ironMan}; making a copy like this works but ,does't work if we have another object inside another
+//because when we modify spiderMan.address.city to New York the city atribute in ironMan would change to new york
+//so basically we don't want that so instead wue should make a copy with structuredClone(ironMan)
 
 
 
-console.log(person,spiderMan)
+console.log(ironMan,spiderMan);
