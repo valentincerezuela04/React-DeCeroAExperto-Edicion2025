2d647c9ad5e9d753c19842eaca2a639c194e43a3
valentincerezuela04 <valentincerezuela04@gmail.com>
2025-08-16 16:36:12 -0300

    const & let variables

---
+++ 01-reforzamiento/src/bases/01-const-let.ts
@@ -0,0 +1,12 @@
+//let and const:
+const firstName: string = 'valentin';
+let lastName = 'cerezuela';
+
+console.log(firstName,lastName);
+
+let diceNumber = 5;
+diceNumber = 3;
+
+const containsLetterV = firstName.includes('v');
+console.log({containsLetterV,diceNumber});
+
