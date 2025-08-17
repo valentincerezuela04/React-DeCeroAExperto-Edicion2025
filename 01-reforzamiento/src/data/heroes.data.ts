77937473b4e35bca970cd473c8d4c57b4074b7be
valentincerezuela04 <valentincerezuela04@gmail.com>
2025-08-17 10:01:22 -0300

    heroes.data.js

---
+++ 01-reforzamiento/src/data/heroes.data.ts
@@ -0,0 +1,44 @@
+interface Heroo{
+    id: number;
+    name: string;
+    owner: Owner;
+}
+
+//type Owner = "DC"| "Marvel"; //basically is a enum like in Java
+enum Owner{
+  DC = 'DC', //0 by default without a assign value
+  Marvel ='Marvel', //1
+}
+
+const heroes: Heroo[] = [
+    {
+      id: 1,
+      name: 'Batman',
+      owner: Owner.DC,
+    },
+    {
+      id: 2,
+      name: 'SpiderMan',
+      owner: Owner.Marvel,
+    },
+    {
+      id: 3,
+      name: 'Superman',
+      owner: Owner.DC,
+    },
+    {
+      id: 4,
+      name: 'Flash',
+      owner: Owner.DC,
+    },
+    {
+      id: 5,
+      name: 'Wolverine',
+      owner: Owner.Marvel,
+    },
+    {
+      id:5,
+      name: 'Green Lantern',
+      owner: Owner.DC,
+    }
+  ];
