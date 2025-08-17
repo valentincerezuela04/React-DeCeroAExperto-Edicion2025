57a0e60aca24f83e4985050007fc05a44c4a4c01
valentincerezuela04 <valentincerezuela04@gmail.com>
2025-08-17 20:06:14 -0300

    GiphyResponseTs & refactor 10-fetch-api.ts

--- 01-reforzamiento/src/bases/10-fetch-api.ts
+++ 01-reforzamiento/src/bases/10-fetch-api.ts
@@ -1,17 +1,34 @@
+import { GiphyResponseTs } from "../data/giphy.response";
+
 const API_KEY = 'upTbYDihY4hEftcqEDlkqgNBifH4gbMd'
 
 const request = fetch(`https://api.giphy.com/v1/stickers/random?api_key=${API_KEY}`)
 
-request
-.then( (response) => response.json())
-.then((data) => { 
-    const imageUrl = data.data.images.original.url;
-    console.log(imageUrl);
 
+const createImgInsideDOM = (url:string) => { 
+    
     const img = document.createElement('img');
-    img.src = imageUrl;
+    img.src = url;
 
     document.body.append(img);
+ }
+request
+.then( (response) => response.json())
+// .then((data) => { 
+//     const imageUrl = data.data.images.original.url;
+//     console.log(imageUrl);
+
+//     const img = document.createElement('img');
+//     img.src = imageUrl;
+
+//     document.body.append(img);
+// })
+
+.then((data:GiphyResponseTs) => { 
+    const imageUrl = data.data.images.original.url;
+    console.log(imageUrl);
+    createImgInsideDOM(imageUrl);
+
 })
 .catch((err) => {
     console.error(err);
