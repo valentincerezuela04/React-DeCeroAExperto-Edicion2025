96605109ff664b5032e4176659d7ea5f384ebb33
Valentin Cerezuela <123900495+valentincerezuela04@users.noreply.github.com>
2025-08-19 14:03:30 -0300

    FirstStesosApp / component

---
+++ 02-first-steps-react/src/FirstStepsApp.tsx
@@ -0,0 +1,15 @@
+export const FistStepsApp = () => {
+  return (
+    <>
+      <h1>Hello world</h1>
+      <p>this is text</p>
+
+      <button onClick={alert}>Click me</button>
+
+
+      <div>
+        <h2>i'm inside a div</h2>
+      </div>
+    </>
+  );
+}
--- 02-first-steps-react/src/main.tsx
+++ 02-first-steps-react/src/main.tsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { FistStepsApp } from './FirstStepsApp'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <h1>Hello world</h1>
+    <FistStepsApp />
+
   </StrictMode>,
 )
