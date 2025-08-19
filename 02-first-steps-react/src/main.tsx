84106bece6064d3b706c3eb14715ca09f93d6edc
Valentin Cerezuela <123900495+valentincerezuela04@users.noreply.github.com>
2025-08-19 14:46:04 -0300

    MyAwesomeApp /impression variables & css styles

--- 02-first-steps-react/src/main.tsx
+++ 02-first-steps-react/src/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { FistStepsApp } from './FirstStepsApp'
+import { MyAwesomeApp } from './MyAwesomeApp'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <FistStepsApp />
-
+    {/* <FistStepsApp /> */}
+    <MyAwesomeApp />
   </StrictMode>,
 )
