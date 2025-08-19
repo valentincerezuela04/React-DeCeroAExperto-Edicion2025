d08351bd1402da95df3be94323cbc98b694d2827
Valentin Cerezuela <123900495+valentincerezuela04@users.noreply.github.com>
2025-08-19 16:09:45 -0300

    handleCliks

--- 02-first-steps-react/src/shopping-cart/ItemCounter.tsx
+++ 02-first-steps-react/src/shopping-cart/ItemCounter.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 
 interface Props {
@@ -24,8 +24,24 @@ interface Props {
 
 //destructuring version :
 
+
+
+
 export const ItemCounter = ({ name, quantity }: Props) => {
-    console.log(quantity);
+    const [count, setCount] = useState(0);
+
+    const handleAdd = () => {
+        setCount(count + 1);
+    };
+
+    const handleSubstract = () => {
+        if (count <= 0) {
+            throw new Error("it can't be a negative number ");
+        }
+
+        setCount(count - 1);
+    }
+
     return (
         <section style={{
             display: 'flex',
@@ -34,9 +50,9 @@ export const ItemCounter = ({ name, quantity }: Props) => {
             marginTop: 10,
         }}>
             <span>{name}</span>
-            <button>+1</button>
-            <span style={{ color: '#fefefe' }}>{quantity}</span>
-            <button>-1</button>
+            <button onClick={handleAdd}>+1</button>
+            <span style={{ color: '#fefefe' }}>{count}</span>
+            <button onClick={handleSubstract}>-1</button>
         </section>
     );
 };
