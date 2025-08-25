7c64b0c626eb12ec1292fc1c8c142d5f11e71c00
Valentin Cerezuela <123900495+valentincerezuela04@users.noreply.github.com>
2025-08-24 23:13:16 -0300

    styles & mockGifs

--- 03-gifs-app/src/main.tsx
+++ 03-gifs-app/src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
+import './index.css'
+import { GifsApp } from './GifsApp'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <h1>Hello world</h1>
+    <GifsApp />
   </StrictMode>,
 )
