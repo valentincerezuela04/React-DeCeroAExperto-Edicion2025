c89f0c4ae12b7ee0804c09940b6cb21daccc7836
Valentin Cerezuela <123900495+valentincerezuela04@users.noreply.github.com>
2025-08-25 20:48:10 -0300

    GifsList

--- 03-gifs-app/src/GifsApp.tsx
+++ 03-gifs-app/src/GifsApp.tsx
@@ -3,6 +3,7 @@ import { mockGifs } from './mock-data/gifs.mock'
 import { CustomHeader } from './shared/components/CustomHeader'
 import { SearchBar } from './shared/components/SearchBar'
 import { PreviousSearches } from './gifs/previousSearches'
+import { GifsList } from './gifs/GifsList'
 export const GifsApp = () => {
     return (
         <>
@@ -14,17 +15,7 @@ export const GifsApp = () => {
 
             <PreviousSearches />
 
-            <div className='gifs-container'>
-                {mockGifs.map((gif) => (
-                    <div key={gif.id} className='gif-card'>
-                        <img src={gif.url} alt={gif.title} />
-                        <h3>{gif.title}</h3>
-                        <p>
-                            {gif.width}x{gif.height} (1.5mb)
-                        </p>
-                    </div>
-                ))}
-            </div>
+            <GifsList gifs={mockGifs} />
 
 
         </>
---
+++ 03-gifs-app/src/gifs/GifsList.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import type { Gif } from '../mock-data/gifs.mock'
+
+
+interface Props {
+    gifs: Gif[];
+}
+export const GifsList = ({ gifs }: Props) => {
+    return (
+        <div className='gifs-container'>
+            {gifs.map((gif) => (
+                <div key={gif.id} className='gif-card'>
+                    <img src={gif.url} alt={gif.title} />
+                    <h3>{gif.title}</h3>
+                    <p>
+                        {gif.width}x{gif.height} (1.5mb)
+                    </p>
+                </div>
+            ))}
+        </div>
+    )
+}
